7f5ead7f180839dd46728a43de9c0fdfd1535f03
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-01 14:16:00 -0400

    add

--- recursion.rb
+++ recursion.rb
@@ -3,5 +3,18 @@ def range(first, last) # (1, 5)
     range(first + 1, last).unshift(first)
   
 end
+def exp(b,n)
+    return  1 if n == 0
+    return b if n== 1
+    b * exp(b,n - 1) 
+end
+def exp1(b,n)
+    return  1 if n == 0
+    return b if n== 1
+    return b * b  if n==2
+    exp1(b,n/2) * exp1(b,n/2) if n.even? 
+    b * exp1(b,(n- 1)/2) * exp1(b,(n - 1)/2) if n.odd?
+end
+p exp1(3,5)
+p exp1(3,11)
 
-p range(1, 5)
